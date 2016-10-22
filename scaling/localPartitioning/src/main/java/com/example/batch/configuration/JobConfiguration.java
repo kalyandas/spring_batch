7b0014c752d88fb01b6d7a117c115defa2554ee5
kalyandas <kalyandas75@gmail.com>
2016-10-22 23:12:55 +0530

    Local Partitioning

---
+++ scaling/localPartitioning/src/main/java/com/example/batch/configuration/JobConfiguration.java
@@ -0,0 +1,117 @@
+package com.example.batch.configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import com.example.batch.domain.ColumnRangePartitioner;
+import com.example.batch.domain.CustomerRowMapper;
+import com.example.batch.domain.Customer;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
+import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+public class JobConfiguration {
+
+	@Autowired
+	public JobBuilderFactory jobBuilderFactory;
+
+	@Autowired
+	public StepBuilderFactory stepBuilderFactory;
+
+	@Autowired
+	public DataSource dataSource;
+
+	@Bean
+	public ColumnRangePartitioner partitioner() {
+		ColumnRangePartitioner columnRangePartitioner = new ColumnRangePartitioner();
+
+		columnRangePartitioner.setColumn("id");
+		columnRangePartitioner.setDataSource(this.dataSource);
+		columnRangePartitioner.setTable("customer");
+
+		return columnRangePartitioner;
+	}
+
+	@Bean
+	@StepScope
+	public JdbcPagingItemReader<Customer> pagingItemReader(
+			@Value("#{stepExecutionContext['minValue']}")Long minValue,
+			@Value("#{stepExecutionContext['maxValue']}")Long maxValue) {
+		System.out.println("reading " + minValue + " to " + maxValue);
+		JdbcPagingItemReader<Customer> reader = new JdbcPagingItemReader<>();
+
+		reader.setDataSource(this.dataSource);
+		reader.setFetchSize(1000);
+		reader.setRowMapper(new CustomerRowMapper());
+
+		MySqlPagingQueryProvider queryProvider = new MySqlPagingQueryProvider();
+		queryProvider.setSelectClause("id, firstName, lastName, birthdate");
+		queryProvider.setFromClause("from customer");
+		queryProvider.setWhereClause("where id >= " + minValue + " and id < " + maxValue);
+
+		Map<String, Order> sortKeys = new HashMap<>(1);
+
+		sortKeys.put("id", Order.ASCENDING);
+
+		queryProvider.setSortKeys(sortKeys);
+
+		reader.setQueryProvider(queryProvider);
+
+		return reader;
+	}
+
+	@Bean
+	@StepScope
+	public JdbcBatchItemWriter<Customer> customerItemWriter() {
+		JdbcBatchItemWriter<Customer> itemWriter = new JdbcBatchItemWriter<>();
+
+		itemWriter.setDataSource(this.dataSource);
+		itemWriter.setSql("INSERT INTO NEW_CUSTOMER VALUES (:id, :firstName, :lastName, :birthdate)");
+		itemWriter.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider());
+		itemWriter.afterPropertiesSet();
+
+		return itemWriter;
+	}
+
+	@Bean
+	public Step step1() throws Exception {
+		return stepBuilderFactory.get("step1")
+				.partitioner(slaveStep().getName(), partitioner())
+				.step(slaveStep())
+				.gridSize(4)
+				.taskExecutor(new SimpleAsyncTaskExecutor())
+				.build();
+	}
+
+	@Bean
+	public Step slaveStep() {
+		return stepBuilderFactory.get("slaveStep")
+				.<Customer, Customer>chunk(1000)
+				.reader(pagingItemReader(null, null))
+				.writer(customerItemWriter())
+				.build();
+	}
+
+	@Bean
+	public Job job() throws Exception {
+		return jobBuilderFactory.get("job")
+				.start(step1())
+				.build();
+	}
+}
